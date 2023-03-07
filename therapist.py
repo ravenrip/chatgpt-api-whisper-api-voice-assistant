655d4c7d0a843969d66995b3acd5ca2ea7183654
Greg Jones <greg@lg2solutions.com>
2023-03-07 12:20:57 -0500

    updated with my prompts

--- therapist.py
+++ therapist.py
@@ -1,8 +1,20 @@
+import platform
+import subprocess
+
 import gradio as gr
-import openai, config, subprocess
+import openai
+
+import config
+
 openai.api_key = config.OPENAI_API_KEY
 
-messages = [{"role": "system", "content": 'You are a therapist. Respond to all input in 25 words or less.'}]
+messages = [
+    {
+        "role": "system",
+        "content": "You are a recovering alcholic in Alcholics Anonymous. Respond to all input in 25 words or less in the form of a rap by Jay-Z.",
+    }
+]
+
 
 def transcribe(audio):
     global messages
@@ -17,14 +29,17 @@ def transcribe(audio):
     system_message = response["choices"][0]["message"]
     messages.append(system_message)
 
-    subprocess.call(["say", system_message['content']])
+    system = platform.system()
 
     chat_transcript = ""
     for message in messages:
-        if message['role'] != 'system':
-            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+        if message["role"] != "system":
+            chat_transcript += message["role"] + ": " + message["content"] + "\n\n"
 
     return chat_transcript
 
-ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+
+ui = gr.Interface(
+    fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text"
+).launch()
 ui.launch()
